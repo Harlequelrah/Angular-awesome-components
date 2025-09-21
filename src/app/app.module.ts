d868101fef77bccf3d4a314af9017b77394e7551
Harlequelrah <degbovimax19@gmail.com>
2025-09-21 12:16:56 +0000

    ajout de l'animation listItem à mat-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { SocialMediaRoutingModule } from './social-media/social-media-routing.module';
 import { SharedModule } from './shared/shared.module';
+import { SocialMediaRoutingModule } from './social-media/social-media-routing.module';
 
 @NgModule({
   declarations: [
@@ -18,7 +16,6 @@ import { SharedModule } from './shared/shared.module';
     CoreModule,
     SharedModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
     SocialMediaRoutingModule,
   ],
   providers: [
--- src/app/shared/components/comments/comments.component.ts
+++ src/app/shared/components/comments/comments.component.ts
@@ -1,14 +1,38 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Comment } from '../../../core/models/comment.model';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 @Component({
   selector: 'app-comments',
   standalone: false,
   templateUrl: './comments.component.html',
-  styleUrl: './comments.component.scss'
+  styleUrl: './comments.component.scss',
+  animations: [
+    trigger('listItem', [
+      state('default', style({
+        'background-color': 'white',
+        'z-index': 1,
+        'transform': 'scale(1)'
+      })),
+      state('active', style({
+        'background-color': 'rgb(201, 157, 242)',
+        'z-index': 2,
+        'transform': 'scale(1.05)'
+      })),
+      transition('default => active', [animate('100ms ease-in-out')]),
+      transition('active => default', [animate('500ms ease-in-out')])
+    ])
+  ]
 })
 export class CommentsComponent implements OnInit {
-  constructor(private formBuilder : FormBuilder){}
+  onMouseLeave() {
+    this.listItemStyleAnimation = 'default';
+  }
+  onMouseEnter() {
+    this.listItemStyleAnimation = 'active';
+  }
+  listItemStyleAnimation: 'default' | 'active' = 'default';
+  constructor(private formBuilder: FormBuilder) { }
   ngOnInit(): void {
     this.commentCtrl = this.formBuilder.control('', [Validators.required, Validators.minLength(10)]);
   }
@@ -22,6 +46,6 @@ export class CommentsComponent implements OnInit {
     }
     this.newComment.emit(this.commentCtrl.value);
     this.commentCtrl.reset();
-}
+  }
 
 }
