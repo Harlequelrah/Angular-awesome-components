7675b0f1696be88b7893d1165629dfddf00ea06a
Harlequelrah <degbovimax19@gmail.com>
2025-09-28 15:52:15 +0000

    création d'animation complexes et réutilisables

--- src/app/shared/components/comments/comments.component.ts
+++ src/app/shared/components/comments/comments.component.ts
@@ -1,13 +1,22 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Comment } from '../../../core/models/comment.model';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { animate, animateChild, group, query, sequence, stagger, state, style, transition, trigger, useAnimation } from '@angular/animations';
+import { flashAnimation } from '../../animations/flash.animation';
+import { slideAndFadeAnimation } from '../../animations/slide-and-fade.animation';
 @Component({
   selector: 'app-comments',
   standalone: false,
   templateUrl: './comments.component.html',
   styleUrl: './comments.component.scss',
   animations: [
+    trigger('list', [
+      transition(':enter', [
+        query('@listItem', [
+          stagger(50, animateChild())
+        ])
+      ])
+    ]),
     trigger('listItem', [
       state('default', style({
         'background-color': 'white',
@@ -21,17 +30,17 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
       })),
       transition('default => active', [animate('100ms ease-in-out')]),
       transition('active => default', [animate('500ms ease-in-out')]),
-      transition('void => *', [style(
-        {
-          transform: 'translateX(-100%)',
-          'background-color': 'rgb(201, 157, 242)',
-          opacity: 0
-        },
-      ), animate('250ms ease-out', style({
-        transform: 'translateX(0)',
-        'background-color': 'white',
-        opacity: 1
-      }))])
+      transition(':enter',
+        [query('.comment-text,.comment-date', [style({ 'opacity': 0 })]),
+        useAnimation(slideAndFadeAnimation, { params: { time: '250ms', startColor: 'rgb(201, 157, 242)' } }),
+        group(
+          [
+            useAnimation(flashAnimation, { params: { time: '250ms', flashColor: 'lightblue' } }),
+            query('.comment-text', [animate('250ms', style({ 'opacity': 1 }))]),
+            query('.comment-date', [animate('500ms', style({ 'opacity': 1 }))])
+          ]
+        )
+        ])
     ])]
 })
 export class CommentsComponent implements OnInit {
